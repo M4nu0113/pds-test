464b1a17eac7f1da2c9f975ff5b6a59433ba8669
Sala K 301 <salak301@salasfr.itm.edu.co>
2024-09-10 08:15:42 -0500

    me obligaron a hacerlo

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp.Definicion;
+using ConsoleApp.Entidades;
+
+var clases = new Clases();
+clases.Ejecutar();
+
+var modelo = new Modelos();
+modelo.Ejecutar();
+/*
+var modeloComplejo = new ModeloComplejo();
+modeloComplejo.Ejecutar();
+*/
